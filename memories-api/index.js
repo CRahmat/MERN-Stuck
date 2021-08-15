046dbb61e733c5b0f2bed6de1aa35b0875e3d6bd
Catur Rahmat <caturrahmat.ctrmt@gmail.com>
2021-08-15 22:28:47 +0700

    Handle CORS Origin Error in Node JS

--- memories-api/index.js
+++ memories-api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const contentRouter = require('./routes/contents')
-
+/*Agar API dapat dihit dari URL manapun selain localhost*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use('/', contentRouter);
 app.listen(4000);
